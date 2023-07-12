96a25885d7fcc9c5200ef52d004ab0dd70fb001a
Michaël <afmichael73@gmail.com>
2023-07-12 19:53:27 +0300

    refactor: promisify, dead code

--- deno/parallel.ts
+++ deno/parallel.ts
@@ -1,14 +1,14 @@
 import { 
   apply_lambda, 
   register_virtual_machine, 
-  register_lambda, PythonApplyInp, PythonRegisterInp
+  register_lambda,
 } from "../bindings/bindings.ts";
   
 
-function promisify(fn: CallableFunction, args: unknown) {
+function promisify<T>(fn: CallableFunction): Promise<T> {
   return new Promise((resolve, reject) => {
     try {
-      resolve(fn(args));
+      resolve(Promise.resolve(fn()));
     } catch (e) {
       reject(e);
     }
@@ -17,29 +17,29 @@ function promisify(fn: CallableFunction, args: unknown) {
 
 const vmName = "myVm";
 console.time("vmInit");
-register_virtual_machine({
+await promisify(() => register_virtual_machine({
   vm_name: vmName,
   preopens: [
     "/app:./deno/python_scripts:readonly"
   ],
   pylib_path: "./vendor/libpython/usr/local/lib",
   wasi_mod_path: "./build/python-wasi-reactor.wasm",
-});
+}));
 console.timeEnd("vmInit");
 
-await promisify(register_lambda, {
+await promisify(() => register_lambda({
   name: "id",
   code: "lambda x: x['a']",
   vm: vmName
-} as PythonRegisterInp);
+}));
 
 const all = [...new Array(100)].map((_, n) => {
-  return promisify(apply_lambda, {
+  return promisify(() => apply_lambda({
     id: n,
     vm: vmName,
     name: "id",
     args: JSON.stringify([{a: "test"}])
-  } as PythonApplyInp);
+  }));
 });
 
 console.time("100 parallel");
@@ -48,11 +48,11 @@ console.timeEnd("100 parallel");
 
 console.time("100 sequential");
 for (let i = 0; i < 100; i++) {
-  await promisify(apply_lambda, {
+  await promisify(() => apply_lambda({
     id: i,
     vm: "myVm",
     name: "id",
     args: JSON.stringify([{a: "test"}])
-  } as PythonApplyInp);
+  }));
 }
 console.timeEnd("100 sequential");
